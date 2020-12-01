b2a38b690e6403c759119637134e94407c1371fe
Alessandro Piseci <piseale03@gmail.com>
2020-12-01 22:27:19 +0100

    salvataggio3

--- ciaobelli/src/main/java/helo.java
+++ ciaobelli/src/main/java/helo.java
@@ -15,6 +15,9 @@ public class helo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int i=0;
+        int e=1;
+        int a=0;
     }
     
 }
